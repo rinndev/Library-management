6293c8c88befe3c049547df174b0a59cfa5ada3c
Rindra Ikhwana <148936838+rinndev@users.noreply.github.com>
2026-07-12 17:11:55 +0700

    Create package.json

---
+++ package.js
@@ -0,0 +1,14 @@
+{
+  "name": "library-management",
+  "version": "1.0.0",
+  "description": "Sistem Manajemen Perpustakaan",
+  "main": "app.js",
+  "scripts": {
+    "start": "node app.js"
+  },
+  "dependencies": {
+    "express": "^4.19.2",
+    "mysql2": "^3.11.0",
+    "cors": "^2.8.5"
+  }
+}
